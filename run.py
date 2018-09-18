2738664a78702f517f048fb0b3fa915f2505e2f0
adi-txt <adithya.met92@gmail.com>
2018-09-18 10:16:38 -0400

    images parsed via the images/ folder

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 '''
 This is where the overall program is run.
 '''
-import sys
+import os
 
 from pkg.parser import get_eyeball_direction
 
@@ -9,17 +9,29 @@ def main() -> None:
     '''
     This is the main function
     '''
-    if len(sys.argv) == 1:
-        print("No images submitted.")
-    else:
-        for arg in sys.argv:
-            if arg != 'run.py':
-                print(
-                    "Image submitted: {0}, Eyeball direction: {1}".format(
-                        arg,
-                        get_eyeball_direction(arg)
-                    )
+
+    path_to_images = os.path.join(os.getcwd(), 'images/')
+    filenames = os.listdir(path_to_images)
+
+    if filenames:
+
+        for file in filenames:
+            file_path = os.path.join(path_to_images, file)
+            # print(file_path)
+            try:
+                direction = get_eyeball_direction(file_path)
+            except: #pylint: disable=bare-except
+                direction = "opencv cannot find eyeballs"
+
+            print(
+                "Image submitted: {0}, Eyeball direction: {1}".format(
+                    file,
+                    direction
                 )
+            )
+    else:
+        print("No images submitted")
+
 
 if __name__ == '__main__':
     main()
